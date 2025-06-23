cc82320c82bcb8dbf75242caf2561d81bfe0d539
Ricardo de Jesus <ricardo05jsantos@gmail.com>
2025-06-23 17:15:00 -0300

    Primeira versão do projeto desafio, com filtragem, posts importados via fetch e modal de visualização de post

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = ({ searchInput, setSearchInput }) => {
+
+  return (
+    <section className={styles.header}>
+      <div className={styles.nav}>
+        <a href='#' className={styles.peachText}>Code</a><a href='#' className={styles.logo}>Lab</a>
+        <input 
+          className={styles.searchBar} 
+          placeholder='Pesquisar no blog'
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
+          />
+      </div>
+    </section>
+  );
+}
+
+export default Header
---
+++ src/components/PostModal.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import styles from './PostModal.module.css'
+
+const PostModal = ({post, isActive, close}) => {
+  const handleClick = (event) => {
+    if (event.target === event.currentTarget) {
+      close();
+    }
+  };
+
+  return (
+    <section className={`${isActive ? styles.modalContainer  : styles.inactive }`} onClick={handleClick} >
+      <div className={styles.modal}>
+        <img className={styles.thumbnail} src={post.imagem_capa}></img>
+        <p className={styles.likeButton}>like</p>
+        <div className={styles.boxUpperPart}>
+          <h2 className={styles.title}>{post.titulo}</h2>
+          <p className={styles.date}>{post.data}</p>
+        </div>
+        <p className={styles.content}>{post.conteudo}</p>
+      </div>
+    </section>
+  )
+}
+
+export default PostModal
