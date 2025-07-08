6d650adbf6c3c5d0c73c8b07565e1f5852422f56
Ricardo <ricardo05jsantos@gmail.com>
2025-07-08 14:50:48 -0300

    botão de liked consertado com useContext

--- src/components/Like.jsx
+++ src/components/Like.jsx
@@ -1,20 +1,36 @@
 import React from "react"
+import { GlobalContext } from "./GlobalPosts"
 import HeartFilled from "../Icons/HeartFilled"
 import HeartUnfilled from "../Icons/HeartUnfilled"
 
 const Like = ({id, style}) => {
-  const [isLiked, setIsLiked] = React.useState(() => {
-    const value = localStorage[id] === 'true' ? true : false;
-    return value;
-  });
+  const {likedPosts, updateLikedPosts} = React.useContext(GlobalContext);
+  /* Puxa likedPosts e updateLikedPosts do GlobalContext */
+  const [isLiked, setIsLiked] = React.useState(false);
+  /* Define um estado reativo na variável isLiked */
+
+  React.useEffect(() => {
+    const liked = likedPosts.find((item) => item === id);
+    if (liked) {
+      setIsLiked(true)
+    } else {
+      setIsLiked(false)
+    }
+  }, [likedPosts])
+  /* Usa um useEffect toda vez que likedPosts mudar*/
+  /* Guarda em liked o id de um post CASO ele seja encontrado na lista likedPosts*/
+  /* Se estiver na lista, define isLiked como verdadeiro, se não, define como falso */
 
   const handleClick = (event) => {
     event.stopPropagation();
 
-    const newIsLiked = !isLiked
-    setIsLiked(newIsLiked);
+    const newList = isLiked ? likedPosts.filter((item) => item !== id) : [...likedPosts, id];
+    /* Guarda na variavel newList o seguinte: */
+    /* Se isLiked for verdadeiro, retorna uma lista filtrada de likedPosts com só os posts diferentes do que foi clicado para remove-lo de lá */
+    /* Se isLiked for falso, retorna likedPosts com o post atual adicionado */
 
-    localStorage[id] = newIsLiked.toString();
+    updateLikedPosts(newList)
+    /* Guarda newList no localStorage */
   }
 
   return (
