152cd32b4afabfa2c8e7c2081e45c39cbd0fdc37
Ricardo de Jesus <ricardo05jsantos@gmail.com>
2025-07-23 17:49:34 -0300

    login-com-rhf-e-validação-com-zod

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react"
-import { supabase } from "../utils/supabase"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
-import { Label } from "@/components/ui/label"
+import { useState } from "react";
+import { supabase } from "../utils/supabase";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 import {
   Card,
   CardContent,
@@ -11,27 +11,40 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { NavLink } from "react-router-dom"
+} from "@/components/ui/card";
+import { NavLink } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod"
 
 
 
 const Login = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
   const [error, setError] = useState<string | null>(null)
   const [success, setSuccess] = useState(false)
   const [loading, setLoading] = useState(false)
 
-  const handleLogin = async (e: React.MouseEvent) => {
-    e.preventDefault()
+  const loginSchema = z.object({
+    email: z.string().email('Formato de e-mail inválido.').min(1, 'E-mail é obrigatório.'),
+    password: z.string().min(6, {
+      message: "Senhas precisam de, no mínimo, 6 dígitos.",
+    }),
+  })
+
+  type LoginSchema = z.infer<typeof loginSchema>
+
+  const { register, handleSubmit, formState: { errors }, } = useForm<LoginSchema>({
+    resolver: zodResolver(loginSchema)
+  })
+
+  const handleLogin = async (data: LoginSchema) => {
     setLoading(true)
     setError(null)
 
     try {
       const { error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
+        email: data.email,
+        password: data.password,
       })
       if (error) throw error
       // Update this route to redirect to an authenticated route. The user already has an active session.
@@ -43,28 +56,28 @@ const Login = () => {
     }
   }
 
-  const handleRegister = async (e: React.FormEvent) => {
-    e.preventDefault()
-    setError(null)
+  // const handleRegister = async (e: React.FormEvent) => {
+  //   e.preventDefault()
+  //   setError(null)
 
-    setLoading(true)
+  //   setLoading(true)
 
-    try {
-      const { error, data } = await supabase.auth.signUp({
-        email,
-        password,
-      })
+  //   try {
+  //     const { error, data } = await supabase.auth.signUp({
+  //       email,
+  //       password,
+  //     })
 
-      console.log('register data: ', data)
+  //     console.log('register data: ', data)
 
-      if (error) throw error
-      setSuccess(true)
-    } catch (error: unknown) {
-      setError(error instanceof Error ? error.message : 'An error occurred')
-    } finally {
-      setLoading(false)
-    }
-  }
+  //     if (error) throw error
+  //     setSuccess(true)
+  //   } catch (error: unknown) {
+  //     setError(error instanceof Error ? error.message : 'An error occurred')
+  //   } finally {
+  //     setLoading(false)
+  //   }
+  // }
 
   return (
     <div className='flex justify-center mt-[220px]'>
@@ -79,20 +92,24 @@ const Login = () => {
           <CardDescription>Realize o login ou se cadastre para acessar o blog</CardDescription>
         </CardHeader>
         <CardContent>
-          <Label className='mb-2' htmlFor="email">Endereço de E-mail</Label>
-          <Input type='email' id='email' placeholder='example@email.com' onChange={({ target }) => { setEmail(target.value) }} />
-          <Label className='mb-2 mt-5' htmlFor="password">Senha</Label>
-          <Input type='password' id='password' onChange={({ target }) => { setPassword(target.value) }} />
+
+          <form onSubmit={handleSubmit(handleLogin)}>
+            <Label className='mb-2' htmlFor="email">Endereço de E-mail</Label>
+            <Input type='email' id='email' {...register('email')}placeholder='example@email.com'/>
+            {errors.email && <p className="text-[12px] text-red-500 mt-1">{errors.email.message}</p>}
+            <Label className='mb-2 mt-5' htmlFor="password">Senha</Label>
+            <Input type='password' id='password' {...register('password')}/>
+            {errors.password && <p className="text-[12px] text-red-500 mt-1">{errors.password.message}</p>}
+            <Button type='submit' disabled={loading} className='font-bold cursor-pointer mt-5'>Entrar</Button>
+            <Button type='button' variant='link' disabled={loading} className='font-bold cursor-pointer mt-5 ml-3'>Não tenho uma conta</Button>
+          </form>
+
         </CardContent>
-        <CardFooter>
-          <Button type='submit' onClick={handleLogin} disabled={loading} className='font-bold cursor-pointer'>Entrar</Button>
-          <Button type='submit' onClick={handleRegister} disabled={loading} variant='outline' className="mx-4 cursor-pointer">Cadastrar</Button>
-        </CardFooter>
         {
-          error && <p className="ml-7 text-red-500">{error}</p>
+          error && <p className="text-[12px] ml-7 text-red-500">{error}</p>
         }
         {
-          success && <p className="ml-7 text-green-500">{success}</p>
+          success && <p className="text-[12px] ml-7 text-green-500">{success}</p>
         }
       </Card >
     </div>
