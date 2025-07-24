8e7f287e1b898cd0ed3e8a5bbff744fe93dac4c0
Ricardo <ricardo05jsantos@gmail.com>
2025-07-24 14:00:58 -0300

    pagina-de-cadastro-separada

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import { GlobalPosts } from './components/GlobalPosts.js';
 import Home from './pages/Home.js'
 import Fav from './pages/Fav.js'
 import Login from './pages/Login.js';
+import Register from './pages/Register.js';
 import Posting from './pages/Posting.js';
 
 import './App.css';
-
 
 const App = () => {
 
@@ -18,6 +18,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Fav />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/posting" element={<Posting />} />
         </Routes>
       </BrowserRouter>
